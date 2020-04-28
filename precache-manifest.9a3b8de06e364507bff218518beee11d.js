2c2a64f8c2d968b59f39659e416725c05bff30cd
1653020 <46862344+1653020@users.noreply.github.com>
2020-04-28 16:46:46 +0700

    Updates

---
+++ precache-manifest.9a3b8de06e364507bff218518beee11d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6234da9e21addace1eff48fded293d07",
+    "url": "/CovidMap/index.html"
+  },
+  {
+    "revision": "21992146d41ebed37503",
+    "url": "/CovidMap/static/css/main.fb90fa60.chunk.css"
+  },
+  {
+    "revision": "aab992d3aaa27562a32d",
+    "url": "/CovidMap/static/js/2.6cc7d766.chunk.js"
+  },
+  {
+    "revision": "92f320287b4a6131c85a8034a67626e7",
+    "url": "/CovidMap/static/js/2.6cc7d766.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21992146d41ebed37503",
+    "url": "/CovidMap/static/js/main.4803ecfc.chunk.js"
+  },
+  {
+    "revision": "65581f8d17708eacd7c2",
+    "url": "/CovidMap/static/js/runtime-main.191382ad.js"
+  }
+]);
